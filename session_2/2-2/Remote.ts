3a1f77a6cb058d5c88dc7930ebc8becd6c678df6
Nash-BETA <nash.0cal@gmail.com>
2021-01-21 10:19:25 +0900

    Promiseとアロー関数について学び

--- session_2/2-2/Remote.ts
+++ session_2/2-2/Remote.ts
@@ -1,20 +1,23 @@
 import {DogDoor} from './DogDoor'
 
 export class Remote{
-
     private openFlag: DogDoor;
+
     public constructor(door: DogDoor){
         this.openFlag = door;
     }
 
-    public pressButton() {
+    public pressButton():void{
         console.log('リモコンのボタンを押したよ');
-        console.log(this.openFlag.isOpen());
 
         if(this.openFlag.isOpen()){
             this.openFlag.close();
         }else{
             this.openFlag.open();
+            //自動でドアがします
+            setTimeout(() => {
+                this.openFlag.close();
+            }, 5000)
         }
     }
 }
--- session_2/2-2/main.ts
+++ session_2/2-2/main.ts
@@ -3,13 +3,28 @@ import { Remote } from './Remote'
 
 class Main{
 
+    private dogDoor = new DogDoor();
+    private remote: Remote = new Remote(this.dogDoor);
+
     public main() {
-        let dogDoor = new DogDoor();
-        let remote:Remote = new Remote(dogDoor);
-        remote.pressButton();
-        remote.pressButton();
-        remote.pressButton();
+        //自動でドアがします
+        this.start('Fidoが外に出たいと吠えた！').then(() =>{
+            this.start('Fidoが外に出たがってる！')
+        })
     }
+
+
+    public async start(text:string){
+        return new Promise((resolve) => {
+            console.log(text)
+            this.remote.pressButton()
+
+            setTimeout(() => {
+                resolve('');
+            }, 5001);
+        })
+    }
+
 }
 
 let m = new Main();
