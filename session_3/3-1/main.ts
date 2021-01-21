9915aa601bb4106418c800af2fe5cd7a5cca3806
Nash-BETA <nash.0cal@gmail.com>
2021-01-21 18:12:49 +0900

    openメソッドに自動で閉じる処理をまとめた、吠えたときの実行ファイルの作成

---
+++ session_3/3-1/main.ts
@@ -0,0 +1,31 @@
+import { DogDoor } from './DogDoor'
+import { Remote } from './Remote'
+
+class Main{
+
+    private dogDoor = new DogDoor();
+    private remote: Remote = new Remote(this.dogDoor);
+
+    main() {
+        //自動でドアがします
+        this.start('Fidoが外に出たいと吠えた！').then(() =>{
+            this.start('Fidoが外に出たがってる！').then(() => {
+                this.start('Fidoが外から戻ってきた！')
+            })
+        })
+    }
+
+    //ボタンを押す実行を順番に
+    start(text:string){
+        return new Promise((resolve) => {
+            console.log(text)
+            this.remote.pressButton()
+            setTimeout(() => {
+                resolve('');
+            }, 501);
+        })
+    }
+}
+
+let m = new Main();
+m.main();
