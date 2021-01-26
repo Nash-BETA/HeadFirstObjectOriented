233d858e8959a4e37cf4e609b82f0fceb7c73c28
Nash-BEAT <nash.0cal@gmail.com>
2021-01-27 00:24:15 +0900

    5session完了

--- session_5/5-2/main.ts
+++ session_5/5-2/main.ts
@@ -1,10 +1,13 @@
 
 import { Instrument } from './Instrument';
 import { GuitarSpec } from './GuitarSpec';
+import { MandolinSpec } from './MandolinSpec';
+import { InstrumentSpec } from './InstrumentSpec';
 import { Inventory } from './Inventory';
 import { Type } from './type'
 import { Wood } from './wood'
 import { Builder } from './builder'
+import { Style } from './style'
 
 class Main {
     public inventory = new Inventory();
@@ -23,32 +26,31 @@ class Main {
         this.inventory.addInstrument('1092', 12995.95, new GuitarSpec(Builder.OLSON, 'SJ', Type.ACOUSTIC, 6, Wood.INDIAN_ROSEWOOD, Wood.CEDAR));
         this.inventory.addInstrument('566-62', 8999.95, new GuitarSpec(Builder.RYAN, 'Cathedral', Type.ACOUSTIC, 6, Wood.COCOBOLO, Wood.CEDAR));
         this.inventory.addInstrument('6 29584', 2100.95, new GuitarSpec(Builder.PRS, 'Dave Navarro Signature', Type.ELECTRIC, 6, Wood.MAHOGANY, Wood.MAPLE));
+        this.inventory.addInstrument('A12', 1234, new MandolinSpec(Builder.RYAN, 'Cathedral', Type.ACOUSTIC, Style.A, Wood.COCOBOLO, Wood.CEDAR));
 
     }
 
-    returnMessage(builder: Builder, model: string, type: Type, numStrings:number,backWood: Wood, topWood: Wood): string {
-        const whatErinLikes = new GuitarSpec(builder, model, type, numStrings,backWood, topWood);
+    returnMessage(instans: InstrumentSpec): string {
+        const whatErinLikes = instans;
 
         let guitars = this.inventory.search(whatErinLikes);
         let result: any = [];
         let guitar: Instrument;
 
-        if (Array.isArray(guitars) || guitars != null) {
+        if (guitars.length > 0) {
             for (guitar of guitars) {
                 let spec = guitar.getSpec();
-
-
                 let resultText = ('君！！！ たぶん ' +
                     Builder.toBuilder(spec.getBuilder()) + ' の ' + spec.getModel() + ' ' +
                     Type.toType(spec.getType()) + ' のギターが好きだと思うよ！！ \n' +
-                    Wood.toWood(spec.getBackWood()) + ' back and sides, \n   ' +
-                    Wood.toWood(spec.getTopWood())+ ' top.\n 今なら $' +
+                    Wood.toWood(spec.getBackWood()) + ' が背面で, ' +
+                    Wood.toWood(spec.getTopWood())+ ' がトップの素材だし！！\n 今なら $' +
                     guitar.getPrice() + '!');
 
-                result.push(resultText);
+                result = resultText;
             }
         } else {
-            result = ('Sorry, Erin, we have nothing for you.');
+            result = 'ごめんよ見当たらなかったよ';
         }
 
         return result;
@@ -56,15 +58,19 @@ class Main {
 
 
     main(): void {
-        const export_text = this.returnMessage(Builder.FENDER, "Stratocastor", Type.ELECTRIC, 6, Wood.ALDER, Wood.ALDER);
+        const export_text = this.returnMessage(new GuitarSpec(Builder.FENDER, "Stratocastor", Type.ELECTRIC, 6, Wood.ALDER, Wood.ALDER));
         console.log(export_text);
 
-        const export_text2 = this.returnMessage( Builder.COLLINGS, "CJ", Type.ACOUSTIC, 6, Wood.INDIAN_ROSEWOOD, Wood.SITKA);
+        const export_text2 = this.returnMessage(new GuitarSpec(Builder.COLLINGS, "CJ", Type.ACOUSTIC, 6, Wood.INDIAN_ROSEWOOD, Wood.SITKA));
         console.log(export_text2);
 
         //みつからないやつ
-        const export_text3 = this.returnMessage(Builder.COLLINGS, "CJ", Type.ELECTRIC, 6, Wood.INDIAN_ROSEWOOD, Wood.SITKA);
+        const export_text3 = this.returnMessage(new GuitarSpec(Builder.COLLINGS, "CJ", Type.ELECTRIC, 6, Wood.INDIAN_ROSEWOOD, Wood.SITKA));
         console.log(export_text3);
+
+
+        const export_text4 = this.returnMessage(new MandolinSpec (Builder.RYAN, 'Cathedral', Type.ACOUSTIC, Style.A, Wood.COCOBOLO, Wood.CEDAR));
+        console.log(export_text4);
     }
 }
 
